350c0b5fad08e67f52c69057bf64bcd83a174f0d
hyein7447 <hyein7447@gmail.com>
2025-05-09 10:15:01 +0900

    작업중

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+
+// ourServices 슬라이드
+var swiper = new Swiper(".servicesSlide", {
+    slidesPerView: 'auto',
+    spaceBetween: 100,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: false,
+    },
+});
+
+// caseStudies 슬라이드
+
+// topSlide
+var swiper = new Swiper("#topSlide", {
+    slidesPerView: '1.5',
+    spaceBetween: 70,
+});
+
+// bottomSlide
+var swiper = new Swiper("#bottomSlide", {
+    slidesPerView: 'auto',
+    slidesPerView: '1.5',
+    spaceBetween: 70,
+});
