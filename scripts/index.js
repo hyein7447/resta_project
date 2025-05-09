2f9ef69c2d5e960260523d96e9f81b22bf771cb0
hyein7447 <hyein7447@gmail.com>
2025-05-09 15:22:39 +0900

    contactus 작업중

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,14 @@
+// 폰트사이즈
+function setFontSize(){
+    const wrap = document.querySelector('.wrap');
+    const wrapWidth = wrap.offsetWidth;
+
+    // #wrap의 width를 기준으로 font-size 설정
+    document.documentElement.style.fontSize = (wrapWidth / 1920) * 16 + 'px';
+}
+window.addEventListener('resize', setFontSize);
+setFontSize();
+
 
 // ourServices 슬라이드
 var swiper = new Swiper(".servicesSlide", {
@@ -8,6 +19,24 @@ var swiper = new Swiper(".servicesSlide", {
         clickable: true,
         dynamicBullets: false,
     },
+});
+
+// ourServices slide4 이중 슬라이드
+var swiperHorizontal = new Swiper(".mySwiperHorizontal", {
+    direction: 'horizontal',
+    nested: true,
+    slidesPerView: 'auto',
+    // freeMode: true,
+    scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+    },
+    mousewheel: true,
+    on: {
+        touchStart: function (swiper, event) {
+            event.stopPropagation();
+        }
+    }
 });
 
 // caseStudies 슬라이드
