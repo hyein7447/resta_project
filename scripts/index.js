371af2e749e8ef89c88616567d893bb6326676c0
hyein7447 <hyein7447@gmail.com>
2025-05-12 15:26:19 +0900

    스크롤트리거 작업중

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,11 @@
 function setFontSize(){
     const wrap = document.querySelector('.wrap');
     const wrapWidth = wrap.offsetWidth;
-
     // #wrap의 width를 기준으로 font-size 설정
     document.documentElement.style.fontSize = (wrapWidth / 1920) * 16 + 'px';
 }
 window.addEventListener('resize', setFontSize);
 setFontSize();
-
 
 // ourServices 슬라이드
 var swiper = new Swiper(".servicesSlide", {
@@ -51,7 +49,6 @@ var swiperHorizontal = new Swiper(".mySwiperHorizontal", {
 });
 
 // caseStudies 슬라이드
-
 // topSlide
 var swiper = new Swiper("#topSlide", {
     slidesPerView: '1.5',
@@ -87,8 +84,64 @@ var swiper = new Swiper("#bottomSlide", {
     },
 });
 
-
 // select option 선택시 컬러변경
 $('select').on('change', function() {
     $(this).css('color', '#fff');
 });
+
+// 메뉴 클릭시 이동
+$(document).ready(function() {
+    $('.gnb a').on('click', function(e) {
+        e.preventDefault(); // 기본 앵커 이동 방지
+
+        // 이동할 대상의 ID를 가져옴
+        var target = $(this).attr('href');
+        
+        // 해당 ID의 위치로 스크롤 이동
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 800); // 800ms 동안 부드럽게 이동
+    });
+});
+
+// caseStudies 스크롤이동 시 동작
+$(document).ready(function () {
+    // GSAP 등록
+    gsap.registerPlugin(ScrollTrigger);
+
+    // GSAP Timeline 생성
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".sldieContent",
+            start: "top 10%", // caseStudies가 화면의 80% 지점에 도달하면 시작
+            end: "center center",
+            toggleActions: "play none none reverse",
+            markers: true 
+        },
+    });
+
+    // topSlide 애니메이션 (왼쪽 → 오른쪽)
+    tl.fromTo("#topSlide .swiper-wrapper", 
+        { x: "33.5625rem", duration: 1.2,}, 
+        { 
+            x: "-6.9375rem", 
+            opacity: 1, 
+            duration: 1.2,
+            ease: "power2.out",
+        },
+    
+    );
+
+    // bottomSlide 애니메이션 (오른쪽 → 왼쪽)
+    tl.fromTo("#bottomSlide .swiper-wrapper", 
+        { x: "-55.4375rem", duration: 1.2,}, 
+        { 
+            x: "0", 
+            opacity: 1, 
+            duration: 1.2,
+            ease: "power2.out"
+        },
+        "+=0.2" // topSlide 완료 후 약간의 간격을 두고 시작
+    );
+});
+
